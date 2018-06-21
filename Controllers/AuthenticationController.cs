06bafe2ebcbcb0b3fb43176a16165e553fb9f5d6
Colin Smith <ColinSmith1@boisestate.edu>
2018-06-21 12:11:06 -0600

    add AuthenticationController.cs

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace UltraliteRedditViewer.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

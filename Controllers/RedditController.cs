fd00c669182145c0f60279aa2c4643b9d9bb81b1
Colin Smith <ColinSmith1@boisestate.edu>
2018-06-21 11:12:15 -0600

    add RedditController.cs

---
+++ Controllers/RedditController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UltraliteRedditViewer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedditController : ControllerBase
+    {
+
+    }
+}

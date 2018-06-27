157d6cdb2149c2356ad230cf892bac9776a14b82
Colin Smith <ColinSmith1@boisestate.edu>
2018-06-27 17:38:13 -0600

    refactor Me and Popular actions to use RefreshTokenWebAgentPool and BotWebAgent, respectively

--- Features/Reddit/RedditController.cs
+++ Features/Reddit/RedditController.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using RedditSharp.Things;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UltraliteRedditViewer.Infrastructure;
 
 namespace UltraliteRedditViewer.Controllers
 {
@@ -12,42 +15,56 @@ namespace UltraliteRedditViewer.Controllers
     public class RedditController : ControllerBase
     {
         private RedditSharp.RefreshTokenWebAgentPool _webAgentPool;
-        private RedditSharp.AuthProvider _authProvider;
+        private IConfiguration _config;
 
-        public RedditController(RedditSharp.RefreshTokenWebAgentPool webAgentPool, RedditSharp.AuthProvider authProvider)
+        public RedditController(RedditSharp.RefreshTokenWebAgentPool webAgentPool, IConfiguration config)
         {
             _webAgentPool = webAgentPool;
-            _authProvider = authProvider;
+            _config = config;
         }
 
         [Route("me")]        
         [HttpGet]
         public async Task<object> Me()
         {
-            object me = null;
-            //get token from header
-            var token = User.Claims.Single(c => c.Type == "Reddit-Access-Token").Value;
-            var reddit = new RedditSharp.Reddit(token);
-            await reddit.InitOrUpdateUserAsync();
-            return reddit.User;
+            var userName = User.Identity.Name;
+            var webAgent = await _webAgentPool.GetOrCreateWebAgentAsync(userName, (name, agent, limit) =>
+            {
+                string refreshToken = User.GetRedditAuthTokens().RefreshToken;
+                name = userName;
+                agent = $"dotnet:UVR:v0.0.1 (by /u/{userName})";
+                limit = RedditSharp.RateLimitMode.Burst;
+                return Task.FromResult(new RedditSharp.RefreshTokenPoolEntry(name, refreshToken, limit, agent));
+            });
+
+            var reddit = new RedditSharp.Reddit(webAgent, true);
+            var listing = reddit.User.GetSubscribedSubreddits();
+            var newestPosts = new List<string>();
+            listing.ForEach(async sr =>
+            {
+                var newestPost = await sr.GetPosts(RedditSharp.Things.Subreddit.Sort.New, 1)
+                .First()
+                .ConfigureAwait(false);
+                newestPosts.Add(newestPost.Title);
+            });
+            return newestPosts;
         }
 
         [Route("popular")]
         [AllowAnonymous]
         [HttpGet]
+        [ResponseCache(Duration = 360, Location = ResponseCacheLocation.Any)]
         public async Task<object> Popular(int pageSize)
         {
-            //var userName = "colinmxs";            
-            //var webAgent = await _webAgentPool.GetOrCreateWebAgentAsync(userName, (uname, uagent, rlimit) =>
-            //{
+            var webAgent = new RedditSharp.BotWebAgent(_config["PopbotUN"], _config["PopbotPW"], _config["RedditClientID-Popbot"], _config["RedditClientSecret-Popbot"], _config["RedditRedirectURI-Popbot"]);
+            var reddit = new RedditSharp.Reddit(webAgent, false);
+            var posts = new List<Post>();
+            await reddit.RSlashAll.GetPosts(10).ForEachAsync(post =>
+            {
+                posts.Add(post);
+            });
 
-            //    uname = userName;
-            //    return Task.FromResult(new RedditSharp.RefreshTokenPoolEntry(uname, accessToken, RedditSharp.RateLimitMode.Burst));
-            //});
-            var result = "";
-            var reddit = new RedditSharp.Reddit(); await reddit.InitOrUpdateUserAsync();
-            //await reddit.RSlashAll.GetPosts(3).ForEachAsync(post => result += post.Title);
-            return result;
+            return posts;
         }
     }
 }

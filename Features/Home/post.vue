b474f167f01dff9c58a4ed2374280e088aa4f60a
Colin Smith <ColinSmith1@boisestate.edu>
2018-07-10 15:13:10 -0600

    implement post

--- Features/Home/post.vue
+++ Features/Home/post.vue
@@ -1,14 +1,59 @@
 <template>
-    <div>        
-        <slot></slot>
+    <div v-if="data.hide != 1">
+        <md-card>
+            <md-card-header>
+                <md-card-header-text>
+                    <div v-on:click="openSelf" class="md-body-1">{{data.title}}</div>
+                    <div class="md-caption">in r/{{data.subreddit}} by u/{{data.author}}</div>
+                </md-card-header-text>
+                <md-card-media v-if="isImgUrl(data.url)">
+                    <img :src="data.thumbnail" v-on:click="toggleImage" />
+                </md-card-media>
+            </md-card-header>
+            <!--<router-link :to="{ name: 'comments', params: { id: data.id }}">Comments</router-link>-->
+        </md-card>
+        <image-viewer v-if="imageExpanded === 1" :url="data.url"></image-viewer>
     </div>
 </template>
 
 <script>
     export default
         {
-            name: 'post'
+            name: 'post',
+            data() {
+                return {
+                    imageExpanded: 0
+                }
+            },
+            props: ['data'],            
+            components: {
+                'image-viewer': () => import('./img')
+            },
+            methods: {
+                isImgUrl(string) {
+                    return /\.(jpg|gif|png)$/.test(string)
+                },
+                isSelf() {
+                    return this.data.is_self && this.data.selftext.length > 0;
+                },
+                openSelf() {
+                    
+                },
+                openExternal(url) {
+                    window.location = url
+                },
+                toggleImage() {
+                    this.imageExpanded === 1 ?
+                        this.imageExpanded = 0 :
+                        this.imageExpanded = 1
+                }
+            }
         }
 </script>
 
-<style scoped></style>
+<style scoped>
+    .md-card {
+        margin: 4px;        
+    }
+    
+</style>

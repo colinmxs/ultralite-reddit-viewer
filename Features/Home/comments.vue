029227bbda8cc123bbb8c791a2208f35797989a1
Colin Smith <ColinSmith1@boisestate.edu>
2018-07-10 15:07:19 -0600

    add comments.vue

---
+++ Features/Home/comments.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <md-card>
+            <md-card-header>
+                <md-card-header-text>
+                    <div class="md-body-1">{{data.title}}</div>
+                    <div class="md-caption">in r/{{data.subreddit}} by u/{{data.author}}</div>
+                </md-card-header-text>
+                <md-card-media v-if="isImgUrl(data.url)">
+                    <img :src="data.thumbnail" v-on:click="toggleImage" />
+                </md-card-media>
+            </md-card-header>
+        </md-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['data']
+    }
+</script>
+
+<style>
+
+</style>

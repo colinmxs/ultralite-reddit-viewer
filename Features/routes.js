414ccea998e99408cca5292ecb975fdcbf74cfd1
Colin Smith <ColinSmith1@boisestate.edu>
2018-07-10 15:07:28 -0600

    add route for comments component

--- Features/routes.js
+++ Features/routes.js
@@ -1,7 +1,9 @@
 const HomePage = () => import('features/Home/home-page')
 const AuthRedirectPage = () => import('features/authentication/auth-redirect-page')
+const Comments = () => import('features/Home/comments')
 
 export const routes = [
     { path: '/', component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
-    { path:'/auth-redirect', component: AuthRedirectPage }
+    { path: '/auth-redirect', component: AuthRedirectPage },
+    { path: '/comments/:id', name:'comments', component: Comments }
 ]
